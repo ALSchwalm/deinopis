8f71aafe306cb5a50f61b06a4ec4eac6253d3524
Adam Schwalm <adamschwalm@gmail.com>
2018-12-01 13:44:38 -0600

    Updates

---
+++ precache-manifest.83c9bf668c3b2451eb8849b45d8d6903.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "045b69e47a57dadef5d0",
+    "url": "/deinopis/site/static/css/main.b4593c64.chunk.css"
+  },
+  {
+    "revision": "045b69e47a57dadef5d0",
+    "url": "/deinopis/site/static/js/main.045b69e4.chunk.js"
+  },
+  {
+    "revision": "ae8c3e37b8b6a5890068",
+    "url": "/deinopis/site/static/js/1.ae8c3e37.chunk.js"
+  },
+  {
+    "revision": "9b33f4404422c73258b5",
+    "url": "/deinopis/site/static/js/runtime~main.9b33f440.js"
+  },
+  {
+    "revision": "a7553a55030cd10f27f570ef09a9a417",
+    "url": "/deinopis/site/index.html"
+  }
+];
