7bcf531daf617af27edfbe0c7c178ee984f13a57
Adam Schwalm <adamschwalm@gmail.com>
2018-12-19 17:51:40 -0600

    Fix CORS for server

--- server.py
+++ server.py
@@ -8,7 +8,9 @@ CORS(app)
 
 @app.route("/suggestions", methods=["POST"])
 def post_suggestion():
-    return jsonify(suggestions(request.json, None))
+    resp = jsonify(suggestions(request.json, None))
+    resp.headers.add('Access-Control-Allow-Origin', '*')
+    return resp
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=8000)
