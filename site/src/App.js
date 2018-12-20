004f2f0406da50ab28db4523dcc8e128d7593003
Adam Schwalm <adamschwalm@gmail.com>
2018-12-19 20:27:09 -0600

    Hide ribbon on small devices

--- site/src/App.js
+++ site/src/App.js
@@ -1,13 +1,33 @@
 import React, { Component } from "react";
+import Hidden from "@material-ui/core/Hidden";
 import TitleBar from "./appbar";
 import SearchGrid from "./searchgrid";
 import ErrorDialog from "./error";
 
 class App extends Component {
   render() {
+    let ribbon = (
+      <Hidden xsDown>
+        <a href="https://github.com/ALSchwalm/deinopis">
+          <img
+            style={{
+              position: "absolute",
+              top: 0,
+              right: 0,
+              border: 0,
+              zIndex: 1299
+            }}
+            src="https://s3.amazonaws.com/github/ribbons/forkme_right_green_007200.png"
+            alt="Fork me on GitHub"
+          />
+        </a>
+      </Hidden>
+    );
+
     return (
       <div className="App">
         <header className="App-header">
+          {ribbon}
           <ErrorDialog />
           <TitleBar />
         </header>
